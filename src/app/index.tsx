c0ffe412367d0518bd9a5984d83275a1d027757a
lbastoss <lucas.bastos.7@hotmail.com>
2026-01-05 15:30:19 -0300

    configurações iniciais do projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+
+export default function Page() {
+  return (
+    <View >
+      <Text>Hello World </Text>
+      <Text>Agora vou fazer app pra celular usando o reacte native.</Text>
+    </View>
+  );
+}
+
+
